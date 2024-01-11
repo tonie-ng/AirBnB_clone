c4bc0232cdb94b75793ca2c3aec06845f243ab97
Tonie Victor <victornwanochi@gmail.com>
2024-01-11 03:55:07 -0800

    added tests for file storage

---
+++ tests/test_engine/test_file_storage.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""Unitests for File Storage"""
+
+import unittest
+import json
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+classes = {
+    "BaseModel": BaseModel,
+}
+
+
+class TestFileStorage(unittest.TestCase):
+    """Unit Tests for File Storage"""
+
+    def test_all(self):
+        """Test the all method associated of the instance"""
+
+        storage = FileStorage()
+        storage_dict = storage.all()
+
+        self.assertIsInstance(storage_dict, dict)
+        self.assertIs(storage_dict, storage._FileStorage__objects)
+
+    def test_new(self):
+        "Test the new method of the instance"
+
+        storage = FileStorage()
+        save = FileStorage._FileStorage__objects
+        FileStorage._FileStorage__objects = {}
+        test_dict = {}
+        for key, value in classes.items():
+            with self.subTest(key=key, value=value):
+                instance = value()
+                instance_key = instance.__class__.__name__ + "." + instance.id
+                storage.new(instance)
+                test_dict[instance_key] = instance
+                self.assertEqual(test_dict, storage._FileStorage__objects)
+        FileStorage._FileStorage__objects = save
+
+    def test_save(self):
+        """Test that save properly saves objects to file.json"""
+
+        storage = FileStorage()
+        new_dict = {}
+        for key, value in classes.items():
+            instance = value()
+            instance_key = instance.__class__.__name__ + "." + instance.id
+            new_dict[instance_key] = instance
+        save = FileStorage._FileStorage__objects
+        FileStorage._FileStorage__objects = new_dict
+        storage.save()
+        FileStorage._FileStorage__objects = save
+        for key, value in new_dict.items():
+            new_dict[key] = value.to_dict()
+        string = json.dumps(new_dict)
+        with open("file.json", "r") as f:
+            js = f.read()
+        self.assertEqual(json.loads(string), json.loads(js))
+
+
+if __name__ == "__main__":
+    unittest.main()
